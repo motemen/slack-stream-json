4e3fed1f6ec2b05ba74f403ba19acea14c733e0f
motemen <motemen@gmail.com>
2019-11-11 21:29:18 +0900

    up

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,25 @@
 use std::env;
 use std::error::Error;
 
-extern crate reqwest;
-extern crate tungstenite;
 extern crate slack_api;
+extern crate tungstenite;
 
 // ref. https://slack-rs.github.io/slack-rs/src/slack/lib.rs.html#147-245
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let token = match env::var("SLACK_TOKEN") {
-        Ok(v) => v,
-        Err(err) => panic!("Error {}", err),
-    };
-    let client = reqwest::Client::new();
+    let token = env::var("SLACK_TOKEN").or(Err("SLACK_TOKEN not set"))?;
+    let client = slack_api::requests::default_client().unwrap();
     let start_response = slack_api::rtm::start(&client, &token, &Default::default())?;
-    let start_url = start_response.url.as_ref().expect("start_url!!!!");
-    let wss_url = reqwest::Url::parse(&start_url)?;
+    let start_url = start_response.url.expect("Could not obtain RTM start_url");
+    let wss_url = url::Url::parse(&start_url)?;
     let (mut websocket, _) = tungstenite::connect(wss_url)?;
 
     loop {
         let message = websocket.read_message()?;
 
         match message {
-            tungstenite::Message::Text(text) => {
-                println!("{}", text)
-            }
+            tungstenite::Message::Text(text) => println!("{}", text),
             _ => {}
         }
     }
-
-    Ok(())
 }
